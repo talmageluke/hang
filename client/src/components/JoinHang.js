4c369ba56efbbd2c159a1d564fe1b23f3d029398
Talmage <35392797+talmageluke@users.noreply.github.com>
2020-11-05 12:42:22 -0700

    revert

--- client/src/components/JoinHang.js
+++ client/src/components/JoinHang.js
@@ -3,7 +3,7 @@ import { Button, Card } from "react-bootstrap";
 
 import axios from 'axios';
 
-export default class PersonList extends React.Component {
+function JoinHang(props) {
   state = {
     hangs: []
   }
@@ -17,26 +17,24 @@ export default class PersonList extends React.Component {
       })
   }
 
-  render() {
-    return (
-      <ul>
-        { this.state.hangs.map(hang =>
-          <Card className="text-center" key={hang.id}>
-            <Card.Header>{hang.User}</Card.Header>
-            <Card.Body>
-              <Card.Title>{hang.event}</Card.Title>
-              <Card.Text>Skill level: {hang.skill}
-              </Card.Text>
-              <Card.Text>Location: {hang.location} ‎‎‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎ Time: {hang.time}</Card.Text>
-              <Card.Text>Details: {hang.details}</Card.Text>
-              <Card.Text> {hang.eventDate}</Card.Text>
-              <Button variant="primary">Join Hang</Button>
-            </Card.Body>
-            <Card.Footer className="text-muted">Created on 5{hang.date}</Card.Footer>
-          </Card>
+  return (
+    <ul>
+      { this.state.hangs.map(hang =>
+        <Card className="text-center" key={hang.id}>
+          <Card.Header>{hang.User}</Card.Header>
+          <Card.Body>
+            <Card.Title>{hang.event}</Card.Title>
+            <Card.Text>Skill level: {hang.skill}
+            </Card.Text>
+            <Card.Text>Location: {hang.location} ‎‎‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎ Time: {hang.time}</Card.Text>
+            <Card.Text>Details: {hang.details}</Card.Text>
+            <Card.Text> {hang.eventDate}</Card.Text>
+            <Button variant="primary">Join Hang</Button>
+          </Card.Body>
+          <Card.Footer className="text-muted">Created on 5{hang.date}</Card.Footer>
+        </Card>
 
-        )}
-      </ul>
-    )
-  }
+      )}
+    </ul>
+  )
 }
